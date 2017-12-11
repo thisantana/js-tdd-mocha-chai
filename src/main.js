5b37fd6d6c9b777b22f76b8adb12585ca91d54e0
Thiago Ferreira de Santana <thiago.fdsantana@gmail.com>
2017-12-11 17:59:37 -0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
-const FizzBuzz = (a) => {
-  if( a === 0) return a;
-  if (a % 3 === 0 && a % 5 === 0) return 'FizzBuzz';
-  if (a % 3 === 0) return 'Fizz';
-  if (a % 5 === 0) return 'Buzz';
 
-  return a;
-};
-
-function foo(a) {
-  return 'teste';
-}
-
-export { FizzBuzz }
